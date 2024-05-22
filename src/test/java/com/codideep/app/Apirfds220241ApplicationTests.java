7e606a1f6b6f9c79dc80c1ae6aa79a469214e4f8
Kevin Arnold Arias Figueroa <kaaf030191@gmail.com>
2024-05-22 16:51:53 -0500

    Init: Inicialización.
    Inicialización general.

---
+++ src/test/java/com/codideep/app/Apirfds220241ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.codideep.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Apirfds220241ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
