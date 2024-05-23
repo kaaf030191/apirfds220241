1dbe4fca5a14cbf86482db38661c4f99626783f0
Kevin Arnold Arias Figueroa <kaaf030191@gmail.com>
2024-05-23 16:59:38 -0500

    Feat: 23-05-2024 - Creación de endpoint inicial.
    Creación del primer servicio y configuración base de CORS.

--- src/main/java/com/codideep/app/Apirfds220241Application.java
+++ src/main/java/com/codideep/app/Apirfds220241Application.java
@@ -5,9 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class Apirfds220241Application {
-
 	public static void main(String[] args) {
 		SpringApplication.run(Apirfds220241Application.class, args);
 	}
-
 }
---
+++ src/main/java/com/codideep/app/business/general/SoIndexGet.java
@@ -0,0 +1,22 @@
+package com.codideep.app.business.general;
+
+public class SoIndexGet {
+	private String message;
+	private String type;
+
+	public String getMessage() {
+		return this.message;
+	}
+
+	public void setMessage(String message) {
+		this.message = message;
+	}
+
+	public String getType() {
+		return this.type;
+	}
+
+	public void setType(String type) {
+		this.type = type;
+	}
+}
