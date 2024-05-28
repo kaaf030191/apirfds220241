6d3e7d8c1b724ddd1951cf8c0a7abe86e0182403
Kevin Arnold Arias Figueroa <kaaf030191@gmail.com>
2024-05-28 15:47:02 -0500

    Feat: 28-05-2024 - Creación de base de datos.
    Creación de script de base de datos.

---
+++ dbscript/db.sql
@@ -0,0 +1,25 @@
+create database dbgeneral;
+use dbgeneral;
+
+create table tperson(
+idPerson char(36) not null,
+firstName varchar(70) not null,
+surName varchar(40) not null,
+dni char(8) not null,
+gender boolean not null,/*true => Masculino, false => Femenino*/
+birthDate date not null,
+createdAt datetime not null,
+updatedAt datetime not null,
+primary key(idPerson)
+) engine=innodb;
+
+create table tcomment(
+idComment char(36) not null,
+idPerson char(36) not null,
+description varchar(1000) not null,
+likeMe int not null,
+createdAt datetime not null,
+updatedAt datetime not null,
+foreign key(idPerson) references tperson(idPerson) on delete cascade on update cascade,
+primary key(idComment)
+) engine=innodb;
